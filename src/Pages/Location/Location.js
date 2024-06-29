bac655de443db772943cecb90fd03e29980adf4e
shabbeer <mdshabbeer707@gmail.com>
2024-06-29 18:31:54 +0530

    copy right removed

--- src/Pages/Location/Location.js
+++ src/Pages/Location/Location.js
@@ -60,7 +60,7 @@ const Location = (props) => {
                 <h1>LOCATION</h1>
             </div>
             <div className="location-box">
-                <iframe title='map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3504.709040886026!2d77.39160357531254!3d28.54846398789373!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e28cc8a0c56b3f9%3A0x53d2b8aa5d54807d!2sOne%20FNG%20Noida!5e0!3m2!1sen!2sin!4v1717222665309!5m2!1sen!2sin"></iframe>
+                <iframe title='map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d112065.65784146429!2d77.31775793374176!3d28.628209341456373!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cefb0610afe8f%3A0x2291398a7376ce83!2sOne%20FNG%20Project!5e0!3m2!1sen!2sin!4v1719666027246!5m2!1sen!2sin"></iframe>
             </div>
         </div>
     )
